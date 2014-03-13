c86fb7353712b2904995d305f46f914c87f06941
saksaer <rollercoasterblue@yahoo.com>
2014-03-12 20:47:50 -0400

    Update cah.js

--- js/service/cah.js
+++ js/service/cah.js
@@ -1,5 +1,7 @@
-var listOfPlayers[];
+var listOfPlayers = [];
 var numPlayers = 0;
+var sentenceCards = [];
+var playerCards = [];
 
 /**
  * Main method for CAH
@@ -24,12 +26,25 @@ function cah(data){
 	if(data.sentenceCard){
 		handleSentenceCard(data);
 	}
-	
-	
-	
 }
 
+/**
+ * Generate the sentence cards
+ */
+playerCards.push("Godzilla");
+playerCards.push("Salamander");
+playerCards = shuffle(playerCards);
+ 
+/**
+ * Generate the player cards
+ */
+sentenceCards.push("_____ just destroyed the city!");
+sentenceCards.push("_____, always good for one last beer.");
+sentenceCards = shuffle(sentenceCards);
 
-
+function shuffle(o){ 
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
 
 
